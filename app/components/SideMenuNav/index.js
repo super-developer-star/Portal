7a5211abcda09bd5c24f950393b50cf9825d8de6
webstar775 <javaguru74@gmail.com>
2017-07-07 02:03:57 +0300

    navigation logo

--- app/components/SideMenuNav/index.js
+++ app/components/SideMenuNav/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 import { LogoutIcon, OverviewIcon, FleetLocationIcon } from 'components/AirstreamIcons';
+import { InlineLogoPNG } from 'components/AirstreamLogo';
 import FontAwesome from 'react-fontawesome';
 import styles from './side-menu-nav.styl';
 import MenuItem from './components/MenuItem';
@@ -9,16 +10,16 @@ import MenuItem from './components/MenuItem';
 export default function SideMenuNav({ collapsed }) {
   return (
     <div className={styles.container}>
-      <div className={styles.menuHeader}>Logo + collapse button</div>
+      <div className={styles.menuHeader}><MenuIcon className={styles.hambugerIcon}/><a href="/"><InlineLogoPNG className={styles.navigationLogo}/></a></div>
       <div className={cx(styles.itemsContainer, { [styles.collapsed]: collapsed })}>
         <ul>
-          <MenuItem collapsed={collapsed} iconComponent={MenuIcon} to="/">Airstream</MenuItem>
+          {/*<MenuItem collapsed={collapsed} iconComponent={MenuIcon} to="/">Airstream</MenuItem>*/}
           <MenuItem collapsed={collapsed} iconComponent={OverviewIcon} to="/">Overview</MenuItem>
           <MenuItem collapsed={collapsed} iconComponent={FleetLocationIcon} to="/fleet">Fleet Location</MenuItem>
         </ul>
 
         <ul>
-          <MenuItem collapsed={collapsed} iconComponent={LogoutIcon} to="/logout">Log out bf kjhfni kjfj jhbj sfbb fdgg</MenuItem>
+          <MenuItem collapsed={collapsed} iconComponent={LogoutIcon} to="/logout">Log out</MenuItem>
         </ul>
       </div>
     </div>
@@ -28,7 +29,7 @@ SideMenuNav.propTypes = {
   collapsed: PropTypes.bool,
 };
 
-const MenuIcon = ({ className }) => <span className={className}><FontAwesome name="bars" size="2x" /></span>;
+const MenuIcon = ({ className }) => <span className={className}><FontAwesome name="bars" size="lg" /></span>;
 MenuIcon.propTypes = {
   className: PropTypes.string,
 };
